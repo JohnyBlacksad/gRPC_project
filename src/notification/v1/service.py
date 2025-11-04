7dec24e3301b7b3b8e444290e4955c250557abad
Artem <vergulofthedevil@gmail.com>
2025-11-05 03:13:20 +0500

    generated dock string

--- src/notification/v1/service.py
+++ src/notification/v1/service.py
@@ -14,12 +14,27 @@ from notification.v1.notification_service_pb2_grpc import (
     add_NotificationServiceServicer_to_server,
 )
 
+# The `Database` class in Python initializes a SQLite database connection with a default path for a
+# notification database, creates a table for notifications if it does not exist, and provides a method
+# to log notification records into the database table.
 class Database:
     def __init__(self, db_path: str = 'notification.db') -> None:
+        """
+        The function initializes a database connection with a default path for a notification database.
+
+        :param db_path: The `db_path` parameter is a string that represents the path to the database
+        file. By default, it is set to `'notification.db'`. This parameter is used to initialize the
+        database path for the class instance, defaults to notification.db
+        :type db_path: str (optional)
+        """
         self.db_path = db_path
         self.init_db()
 
     def init_db(self):
+        """
+        The function initializes a SQLite database by creating a table for notifications if it does not
+        already exist.
+        """
         conn = sqlite3.connect(self.db_path)
 
         try:
@@ -39,6 +54,29 @@ class Database:
             conn.close()
 
     def log_notification(self, notif_type: str, to_addr: str, subject: str, body: str):
+        """
+        The function `log_notification` inserts a notification record into a SQLite database table with
+        specified parameters.
+
+        :param notif_type: The `notif_type` parameter in the `log_notification` function represents the
+        type or category of the notification being logged. It could be a string indicating the purpose
+        or nature of the notification, such as "email", "sms", "alert", "reminder", etc. This parameter
+        helps in organizing and
+        :type notif_type: str
+        :param to_addr: The `to_addr` parameter in the `log_notification` function represents the
+        address or destination where the notification will be sent. This could be an email address,
+        phone number, username, or any other identifier that specifies the recipient of the notification
+        :type to_addr: str
+        :param subject: The `subject` parameter in the `log_notification` function refers to the subject
+        line of the notification email or message that will be logged in the database. It typically
+        contains a brief summary or description of the notification content
+        :type subject: str
+        :param body: The `body` parameter in the `log_notification` function refers to the content or
+        message of the notification that you want to log. This is where you would provide the main
+        information or details that you want to include in the notification being stored in the database
+        :type body: str
+        """
+
         import time
 
         conn = sqlite3.connect(self.db_path)
@@ -57,11 +95,36 @@ class Database:
             conn.close()
 
 
+# The `NotificationService` class in Python initializes with a database instance and provides a method
+# `SendNotification` to log and send notifications.
 class NotificationService(NotificationServiceServicer):
+
     def __init__(self, db: Database):
+        """
+        The `__init__` function initializes an object with a database instance.
+
+        :param db: Database object that will be used for database operations
+        :type db: Database
+        """
+
         self.db = db
 
     def SendNotification(self, request: SendNotificationRequest, context):
+
+        """
+        The function `SendNotification` logs a notification and returns a success response after sending
+        the notification.
+
+        :param request: The `SendNotification` method takes in three parameters:
+        :type request: SendNotificationRequest
+        :param context: The `context` parameter in the `SendNotification` method is typically used to
+        provide additional contextual information or configuration settings for the operation being
+        performed. It can include details such as the user's session information, security credentials,
+        or any other relevant data needed for the operation. In this specific method, the
+        :return: The method `SendNotification` is returning a `SendNotificationResponse` object with the
+        attribute `success` set to `True`.
+        """
+
         logging.info(f'Отправка уведомления:\n Кому: {request.to}\n Тема: {request.subject}\n Тело: {request.body}')
 
         self.db.log_notification(
@@ -74,6 +137,10 @@ class NotificationService(NotificationServiceServicer):
         return SendNotificationResponse(success=True)
 
 def serve():
+    """
+    The `serve` function sets up a gRPC server for a Notification Service on port 50055.
+    """
+
     db = Database('notification.db')
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     add_NotificationServiceServicer_to_server(NotificationService(db), server)
--- src/order/v1/server.py
+++ src/order/v1/server.py
@@ -35,12 +35,31 @@ from notification.v1.notification_service_pb2 import (
 )
 from notification.v1.notification_service_pb2_grpc import NotificationServiceStub
 
+
+
+# The `Database` class in Python initializes a SQLite database connection, creates tables for orders
+# and order items, and provides methods to create new orders with associated items.
 class Database:
     def __init__(self, db_path: str = 'order.db') -> None:
+        """
+        This Python function initializes a database connection using a specified path or a default path
+        if none is provided.
+
+        :param db_path: The `db_path` parameter is a string that represents the path to the database
+        file. By default, it is set to `'order.db'`. This parameter is used to initialize the database
+        path for the class instance, defaults to order.db
+        :type db_path: str (optional)
+        """
+
         self.db_path = db_path
         self.init_db()
 
     def init_db(self):
+        """
+        The `init_db` function creates two tables, `orders` and `order_item`, in a SQLite database if
+        they do not already exist.
+        """
+
         conn = sqlite3.connect(self.db_path)
 
         try:
@@ -72,6 +91,29 @@ class Database:
             conn.close()
 
     def create_order(self, user_id: str, items, total_amount: int, currency_code: str):
+
+        """
+        The function creates a new order in a SQLite database with order details and associated items.
+
+        :param user_id: The `user_id` parameter is a string that represents the unique identifier of the
+        user who is placing the order. It is used to associate the order with a specific user in the
+        database
+        :type user_id: str
+        :param items: The `items` parameter in the `create_order` method is expected to be a collection
+        of objects representing the individual items in the order. Each item object should have the
+        following attributes:
+        :param total_amount: The `total_amount` parameter in the `create_order` function represents the
+        total amount of the order in the specified currency. It is an integer value that indicates the
+        total cost of all items in the order before any discounts or taxes are applied
+        :type total_amount: int
+        :param currency_code: The `currency_code` parameter in the `create_order` function is a string
+        that represents the currency code used for the order. It specifies the currency in which the
+        total amount for the order is denominated. For example, "USD" for US Dollars, "EUR" for Euros,
+        "JP
+        :type currency_code: str
+        :return: The `create_order` method returns the `order_id` of the newly created order.
+        """
+
         order_id = f"ord-{int(time.time())}"
         created_at = int(time.time())
 
@@ -103,12 +145,38 @@ class Database:
         finally:
             conn.close()
 
+# The `OrderService` class in Python defines methods for creating orders by interacting with gRPC
+# services for user, cart, catalog, database, and notifications.
 class OrderService(OrderServiceServicer):
     def __init__(self, db: Database):
+        """
+        The function initializes an object with a database parameter.
+
+        :param db: The parameter `db` in the `__init__` method is of type `Database`. It is used to
+        initialize an instance of the class with a database object
+        :type db: Database
+        """
         self.db = db
 
 
     def CreateOrder(self, request: CreateOrderRequest, context):
+
+        """
+        The function `CreateOrder` processes a new order by interacting with user, cart, catalog,
+        database, and notification services in a gRPC environment.
+
+        :param request: The code you provided seems to be a Python function for creating an order. It
+        interacts with gRPC services for user information, cart details, product catalog, and
+        notifications. Here's a breakdown of the code:
+        :type request: CreateOrderRequest
+        :param context: The `context` parameter in the `CreateOrder` function is typically used in gRPC
+        services to handle errors and manage the lifecycle of the RPC call. It provides methods for
+        aborting the RPC call with a specific status code and message
+        :return: The `CreateOrder` function returns a `CreateOrderResponse` object containing the order
+        details such as order ID, user ID, order status, items in the order, total amount, currency
+        code, and creation timestamp.
+        """
+
         user_id = request.user_id
 
         user_channel = grpc.insecure_channel('localhost:50052')
@@ -198,7 +266,14 @@ class OrderService(OrderServiceServicer):
         )
         return CreateOrderResponse(order=order)
 
+
 def serve():
+
+    """
+    The `serve` function sets up a gRPC server for an Order Service using a database and starts it on
+    port 50054.
+    """
+
     db = Database('order.db')
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     add_OrderServiceServicer_to_server(OrderService(db), server)
